a9164920b06b08fef0da67598cd93938b2ec9001
jpadron1986 <jpadron1986@gmail.com>
2021-02-25 19:00:44 -0500

    main

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,21 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":809},"pages":[{"id":"page-home","displayName":"home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-blacklist","displayName":"blacklist","link":{"linkType":"LinkTypePage","href":"#!page-blacklist"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
+    window.silex.data = {"site":{"width":1000},"pages":[{"id":"page-page-1","displayName":"Page 1","link":{"linkType":"LinkTypePage","href":"#!page-page-1"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-blacklist","displayName":"blacklist","link":{"linkType":"LinkTypePage","href":"#!page-blacklist"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
+
+/*
+ * animation widget from here: https://github.com/silexlabs/Silex/issues/443
+ */
+$(function() {
+    var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    if(!isMobile) {
+        window.sr = ScrollReveal({
+            distance: '100px'
+        });
+        sr.reveal('.from-left', { origin:  'left'});
+        sr.reveal('.from-right', { origin:  'right'});
+        sr.reveal('.from-top', { origin:  'top'});
+        sr.reveal('.from-bottom', { origin:  'bottom'});
+    }
+})
+    
